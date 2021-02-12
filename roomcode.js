1d596340fe797fd9ea4ca341be11619286b911b6
Jesse Stilwell <jesse.stilwell@gmail.com>
2021-02-11 21:40:30 -0500

    Initial commit

---
+++ roomcode.js
@@ -0,0 +1,25 @@
+// Get room code from URL
+const arr = document.URL.match(/code=([a-zA-Z0-9_.-]+)/)
+const roomCode = arr[1]
+const slowCode = roomCode.replace(/(.{1})/g, "$1.-")
+document.getElementById("roomCode").innerHTML = roomCode
+
+// Text to speech settings
+let utter = new SpeechSynthesisUtterance()
+utter.lang = "en-US"
+utter.text = `The room code is <say-as interpret-as="characters">${slowCode}</say-as>`
+utter.volume = 0.7
+utter.rate = 0.25
+
+// Repeat
+var i = 1
+
+function playCode() {
+  setTimeout(function () {
+    window.speechSynthesis.speak(utter)
+    i++
+    if (i < 5) {
+      playCode()
+    }
+  }, 500)
+}
